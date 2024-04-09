66b6bd2ce2d3669ef8f7b62131fc53f9767276af
ErrEBalerdi <ricardobalerdi07122000@gmail.com>
2024-04-09 02:38:42 -0300

    opcion de idioma y puntero
    el puntero puede ser modificado y unicamente utilizar un solo puntero y que se valla moviendo mediante un Script pero al utilizar UI creo que hay muchas opciones de este  tipo limitadas

--- LuciaHub/Assets/Script/MainMenuScript/Text/TextMainMenu.cs
+++ LuciaHub/Assets/Script/MainMenuScript/Text/TextMainMenu.cs
@@ -1,13 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TextMainMenu : MonoBehaviour
 {
+   
+    
+    void MenuSpanish()
+    {
+        TextMeshProUGUI textMeshProJugar = GameObject.Find("TextRunGame").GetComponent<TextMeshProUGUI>();
+        textMeshProJugar.text = "JUGAR";
+        textMeshProJugar.ForceMeshUpdate();
+        TextMeshProUGUI textMeshProOPCIONES = GameObject.Find("TextOptions").GetComponent<TextMeshProUGUI>();
+        textMeshProOPCIONES.text = "OPCIONES";
+        textMeshProOPCIONES.ForceMeshUpdate();
+        TextMeshProUGUI textMeshProSALIR = GameObject.Find("TextQuit").GetComponent<TextMeshProUGUI>();
+        textMeshProSALIR.text = "SALIR";
+        textMeshProSALIR.ForceMeshUpdate();
+    }
+    void MenuEnglish()
+    {
+        TextMeshProUGUI textMeshProJugar = GameObject.Find("TextRunGame").GetComponent<TextMeshProUGUI>();
+        textMeshProJugar.text = "PLAY";
+        textMeshProJugar.ForceMeshUpdate();
+        TextMeshProUGUI textMeshProOPCIONES = GameObject.Find("TextOptions").GetComponent<TextMeshProUGUI>();
+        textMeshProOPCIONES.text = "OPTIONS";
+        textMeshProOPCIONES.ForceMeshUpdate();
+        TextMeshProUGUI textMeshProSALIR = GameObject.Find("TextQuit").GetComponent<TextMeshProUGUI>();
+        textMeshProSALIR.text = "QUIT";
+        textMeshProSALIR.ForceMeshUpdate();
+    }
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { //Ejecuta el lenguaje del menu prinsipal (realizar un if que pueda camviar el idioma desde el menu de opciones)
+        MenuSpanish();
+        //MenuEnglish();
     }
 
     // Update is called once per frame
