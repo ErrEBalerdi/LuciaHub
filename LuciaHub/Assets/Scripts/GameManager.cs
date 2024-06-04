883a8c456e530dc2d5ed546a7681b17603659d42
Kinoniko-v1 <kinoniko123@gmail.com>
2024-06-04 10:29:09 -0300

    Revert "Merge pull request #9 from ErrEBalerdi/RikiBranch"
    This reverts commit 963d6c9b65c1f6364b64d3a38aa4b6b4396032cf, reversing
    changes made to db140a71db0c74241bd65ca6202979c6f6efaace.

--- LuciaHub/Assets/Scripts/GameManager.cs
+++ LuciaHub/Assets/Scripts/GameManager.cs
@@ -1,89 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<Transform> spawnPositions;
-    [SerializeField] private Transform playerTransform;
-
-    [SerializeField] private CameraBehaviour currentCamera;
-
-    private Transform spawnPositionObject;
     // Patron Singleton, permite una sola instancia de este objeto. Si hay otra, este se destruye.
     public static GameManager Instance { get; private set; }
-    public float minX;
-    public float maxX; 
 
     private void Awake()
     {
-        if (Instance != null && Instance != gameObject)
-            Destroy(gameObject);
+        if (Instance != null && Instance != this)
+            Destroy(this);
         else
             Instance = this;
     }
-    private void Start()
-    {
-        // Le paso 0 para indicar la primer posición de la lista provisoriamente
-        SpawnPlayer(0);
-    }
 
-    public void SpawnPlayer(int pos)
-    {
-        // Inicializar las variables: objeto que contiene las posiciones, accedo al transform
-        // Lista de transform la inicializo y luego la relleno con los que tenga el objeto
-
-        spawnPositionObject = GameObject.FindWithTag("SpawnPos").GetComponent<Transform>();
-        if (spawnPositionObject == null)
-            Debug.LogWarning("Spawn Position Object not found.");
-        spawnPositions = new List<Transform>();
-
-        foreach (Transform spawnPoint in spawnPositionObject)
-        {
-            spawnPositions.Add(spawnPoint);
-        }
-
-        // Se asigna el transform.position del spawn al jugador
-        playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
-        if (playerTransform != null)
-            playerTransform.position = spawnPositions[pos].position;
-        else
-            Debug.LogWarning("Player not Found");
-
- 
-    }
-
-    public void LoadMainMenu()
+    void LoadGame()
     {
         SceneManager.LoadScene(0);
     }
-    
-    public void LoadGame()
+    void LoadMainMenu()
     {
-       
+        SceneManager.LoadScene(0);
     }
-    public void EnterRoom(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-        SpawnPlayer(0);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        switch (currentSceneIndex)
-        {
-            case 1:
-
-                minX = -10f;
-                maxX = 15f;
-
-                break;
-          case 2:
-                minX = -1.5f;
-                maxX = 7f;
-                break ;
-        }
-    }
-
 }
