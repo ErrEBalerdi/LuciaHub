3fddd9e4f3d45f3f3b0385fff3b9959c1d9e2365
RicardoBalerdi <ricardobalerdi07122000@gmail.com>
2024-06-04 22:34:20 -0300

    arreglos

--- LuciaHub/Assets/Scripts/InGame/CameraBehaviour.cs
+++ LuciaHub/Assets/Scripts/InGame/CameraBehaviour.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,7 +9,8 @@ public class CameraBehaviour : MonoBehaviour
     [SerializeField] private float offset;
     private float minX;
     private float maxX;
-    private GameManager gameManager;
+
+    public float MinX {  set { value =  minX; } }
 
     private PlayerMovement playerDirection;
     public float smoothTime = 20f;
@@ -19,18 +19,13 @@ public class CameraBehaviour : MonoBehaviour
     void Start()
     {
         playerDirection = target.GetComponent<PlayerMovement>();
-
-        gameManager = FindAnyObjectByType<GameManager>();
-        
-
     }
 
 
     void Update()
     {
-        maxX = gameManager.maxX;
-        minX = gameManager.minX;
-        // SitÃºa la camara un poco a la izquierda o a la derecha del personaje segun donde mire
+     
+        // Sitúa la camara un poco a la izquierda o a la derecha del personaje segun donde mire
 
         // '?' Es un operador ternario (como un if else)
         // Mathf.Abs saca el valor absoluto. Por lo que si le pones un menos antes, es el mismo valor en negativo.
@@ -38,15 +33,15 @@ public class CameraBehaviour : MonoBehaviour
         offset = playerDirection.isFacingRight ? Mathf.Abs(offset) : -Mathf.Abs(offset);
 
         // Almacena el valor de donde debe estar la camara cuando se mueve el jugador
-        Vector3 desiredPosition = new Vector3(playerDirection.transform.position.x + offset,transform.position.y, transform.position.z);
+        Vector3 desiredPosition = new Vector3(target.transform.position.x + offset, transform.position.y, transform.position.z);
 
         // Limita el avance de la camara para no salirse del fondo
-        //float clampedX = Mathf.Clamp(desiredPosition.x, minX , maxX); <--- no anda y creo que no es nesesario 
-        desiredPosition = new Vector3(Mathf.Clamp(desiredPosition.x, minX, 10f), desiredPosition.y, desiredPosition.z);
+        float clampedX = Mathf.Clamp(desiredPosition.x, minX , maxX);
+        desiredPosition = new Vector3(clampedX, desiredPosition.y, desiredPosition.z);
 
 
         //transform.position = desiredPosition;
-        // Esto es el concepto de interpolaciÃ³n. No lo se, la linea la escribio chatpgt xd
+        // Esto es el concepto de interpolación. No lo se, la linea la escribio chatpgt xd
         transform.position = Vector2.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
     }
 }
